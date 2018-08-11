0abed344d16b26e3a30ee23b387399ab089d191a
meenakshi-nagpal <meenakshi.nagpal22@@gmail.com>
2018-08-11 15:11:03 -0400

    new third file

---
+++ src/test_git_project/myThirdClass.java
@@ -0,0 +1,17 @@
+package test_git_project;
+
+public class myThirdClass {
+
+	private String t1;
+
+	public String getT1() {
+		return t1;
+	}
+
+	
+	public void setT1(String t1) {
+		this.t1 = t1;
+	}
+	
+	
+}
