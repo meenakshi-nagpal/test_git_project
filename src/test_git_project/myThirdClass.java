d87d606ef349c84c0ae625c1a04182501aab44a5
meenakshi-nagpal <meenakshi.nagpal22@@gmail.com>
2018-08-11 15:13:47 -0400

    modified third class

--- src/test_git_project/myThirdClass.java
+++ src/test_git_project/myThirdClass.java
@@ -3,7 +3,7 @@ package test_git_project;
 public class myThirdClass {
 
 	private String t1;
-
+	private String t2;
 	public String getT1() {
 		return t1;
 	}
@@ -13,5 +13,7 @@ public class myThirdClass {
 		this.t1 = t1;
 	}
 	
-	
+	public void setT2(String t2) {
+		this.t2 = t2;
+	}
 }
