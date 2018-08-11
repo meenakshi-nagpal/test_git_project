e99b1dd1ef74a8969de973235c5a2aa903b58ba8
meenakshi-nagpal <meenakshi.nagpal22@@gmail.com>
2018-08-11 14:09:04 -0400

    my second file

---
+++ src/test_git_project/mySecondClass.java
@@ -0,0 +1,16 @@
+package test_git_project;
+
+public class mySecondClass {
+
+	private String s1;
+
+	
+	public String getS1() {
+		return s1;
+	}
+
+	public void setS1(String s1) {
+		this.s1 = s1;
+	}
+	
+}
