544ae3816a9ddd5aa07fdba01d0e266876732689
meenakshi-nagpal <meenakshi.nagpal22@@gmail.com>
2018-08-11 14:07:39 -0400

    added getter and setter

--- src/test_git_project/myFirstClass.java
+++ src/test_git_project/myFirstClass.java
@@ -2,4 +2,26 @@ package test_git_project;
 
 public class myFirstClass {
 
+	private String hello;
+	private String hello1;
+	
+	
+	public String getHello() {
+		return hello;
+	}
+	public void setHello(String hello) {
+		this.hello = hello;
+	}
+	public String getHello1() {
+		return hello1;
+	}
+	public void setHello1(String hello1) {
+		this.hello1 = hello1;
+	}
+	
+	@Override
+	public String toString() {
+		return "myFirstClass [hello=" + hello + ", hello1=" + hello1 + "]";
+	}
+	
 }
