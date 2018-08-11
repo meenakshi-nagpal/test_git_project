233e122751e083472224c1861cb44c1a1b9b93bd
meenakshi-nagpal <meenakshi.nagpal22@@gmail.com>
2018-08-11 15:07:54 -0400

    erer

--- src/test_git_project/mySecondClass.java
+++ src/test_git_project/mySecondClass.java
@@ -3,7 +3,7 @@ package test_git_project;
 public class mySecondClass {
 
 	private String s1;
-
+	private String s3;
 	
 	public String getS1() {
 		return s1;
@@ -12,5 +12,11 @@ public class mySecondClass {
 	public void setS1(String s1) {
 		this.s1 = s1;
 	}
-	
+	public String getS3() {
+		return s3;
+	}
+
+	public void setS3(String s3) {
+		this.s3 = s3;
+	}
 }
