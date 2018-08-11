e96c8d1ed7b0b23aeca49b33f45822547823f4a5
meenakshi-nagpal <meenakshi.nagpal22@gmail.com>
2018-08-11 14:11:15 -0400

    aded new string

--- src/test_git_project/mySecondClass.java
+++ src/test_git_project/mySecondClass.java
@@ -3,7 +3,7 @@ package test_git_project;
 public class mySecondClass {
 
 	private String s1;
-
+	private String s2;
 	
 	public String getS1() {
 		return s1;
@@ -11,6 +11,13 @@ public class mySecondClass {
 
 	public void setS1(String s1) {
 		this.s1 = s1;
+	}
+	public String getS2() {
+		return s2;
+	}
+
+	public void setS2(String s2) {
+		this.s2 = s2;
 	}
 	
 }
